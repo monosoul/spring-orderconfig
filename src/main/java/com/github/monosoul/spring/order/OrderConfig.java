95fc3120bb7818277081fc29522f524a945e1005
monosoul <andrei.nevedomskii@n26.com>
2019-09-15 13:27:00 +0200

    Fix javadoc for OrderConfig

--- src/main/java/com/github/monosoul/spring/order/OrderConfig.java
+++ src/main/java/com/github/monosoul/spring/order/OrderConfig.java
@@ -35,6 +35,7 @@ import org.springframework.stereotype.Component;
  * @Configuration
  * public class JavaConfig {
  *
+ *     @Bean
  *     public OrderConfig<SomeInterface> someInterfaceOrderConfig() {
  *         return new OrderConfigImpl<>(Arrays.asList(
  *             OrderConfigItemImpl.of(SomeClass.class),
